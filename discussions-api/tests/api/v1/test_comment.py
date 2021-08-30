6bbb8418a455bf4d75b66bb2d1970462b68ddcab
Matheus Bosa <matheusbozza@gmail.com>
2021-08-29 23:32:59 -0300

    ci: :green_heart: Using Poetry in GitHub Actions

--- discussions-api/tests/api/v1/test_comment.py
+++
@@ -1,6 +0,0 @@
-from fastapi.testclient import TestClient
-
-
-def test_list_comments_by_topic(client: TestClient) -> None:
-    response = client.get("/api/v1/topics/1/comments")
-    assert response.status_code == 200
--- discussions-api/tests/api/v1/test_topic.py
+++
@@ -1,6 +0,0 @@
-from fastapi.testclient import TestClient
-
-
-def test_list_topics(client: TestClient) -> None:
-    response = client.get("/api/v1/topics")
-    assert response.status_code == 200
